60d4dec3e925f73c1b05291e00f6cbd51619b715
aravinth <aravinth1626@gmail.com>
2022-07-22 11:27:53 +0530

    video updated on homepage

--- js/main.js
+++ js/main.js
@@ -185,7 +185,10 @@ jQuery(document).ready(function ($) {
             markup: '<style>.mfp-iframe-holder .mfp-content {max-width: 900px;height:500px}</style>' +
                 '<div class="mfp-iframe-scaler" >' +
                 '<div class="mfp-close"></div>' +
-                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                // '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'
+                // `<iframe src="https://player.vimeo.com/video/337272884?h=cc3fdf852e?autoplay=1& muted=1" frameborder="0" allow="autoplay  fullscreen; picture-in-picture" allowfullscreen></iframe>`
+                `<iframe src="https://player.vimeo.com/video/337272884?h=cc3fdf852e&autoplay=1" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>` 
+                +
                 '</div></div>'
         }
     });
